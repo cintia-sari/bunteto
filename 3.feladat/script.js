53154766a1914dbed0aa35f40b81b6618fe84626
Sari Cintia <sari.cintia1996@gmail.com>
2023-09-06 12:17:07 +0200

    formatting css and add js

---
+++ 3.feladat/script.js
@@ -0,0 +1,22 @@
+const errorMessages = document.querySelector(".error-message");
+const emailInput = document.querySelector(".email-input");
+const questions = document.querySelector(".question");
+const button = document.querySelector(".login");
+
+const validateEmail = (emailInput) => {
+  var regex =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(emailInput).toLowerCase());
+};
+
+emailInput.addEventListener("blur", () => {
+  if (!validateEmail(emailInput.value)) {
+    errorMessages.style.visibility = "visible";
+    questions.style.textDecoration = "underline";
+    emailInput.style.border = "1px solid red";
+  } else {
+    errorMessages.style.visibility = "hidden";
+    questions.style.textDecoration = "none";
+    emailInput.style.border = "1px solid var(--sötét-zöld)";
+  }
+});
